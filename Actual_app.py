155a8b0799a83c60a80472bafd4e036a2bb8f0fc
Eddie9876 <em775404@gmail.com>
2026-01-31 01:05:23 -0600

    changes to flask

--- Actual_app.py
+++ Actual_app.py
@@ -1,13 +1,8 @@
 from flask import Flask, jsonify, render_template, request
 from DepopCalc import program
-app = Flask(__name__) 
+app = Flask(__name__)
 
 
 @app.route("/", methods =['GET', 'POST']) 
-def index(): 
-    if request.method == 'POST': 
-        user_input  = request.form['username'] 
-        return f"Hello, {user_input}" 
-    return render_template("index.html") 
-
-app.run(debug=True)
+def home(): 
+    return render_template('index.html')
